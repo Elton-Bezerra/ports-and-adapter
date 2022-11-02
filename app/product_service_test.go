08d67ec00ba4849e558d4747e28680fdb463ae76
Elton Bezerra <elton.cin@hotmail.com>
2022-11-02 16:29:52 -0300

    Testing ProductService#Get

--- app/product_service_test.go
+++ app/product_service_test.go
@@ -1,1 +1,28 @@
 package app_test
+
+import (
+	"testing"
+
+	"github.com/Elton-Bezerra/ports-and-adapter/app"
+	mock_app "github.com/Elton-Bezerra/ports-and-adapter/app/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_ProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_app.NewMockProductInterface(ctrl)
+	persistence := mock_app.NewMockProductPersistenceInterface(ctrl)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := app.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
