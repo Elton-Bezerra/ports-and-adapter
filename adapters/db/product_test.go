a058a875c520e7e6dcf9e804cf6a623ae18c860b
Elton Bezerra <elton.cin@hotmail.com>
2022-11-02 17:26:55 -0300

    test: starting db adapter tests

---
+++ adapters/db/product_test.go
@@ -0,0 +1,41 @@
+package db_test
+
+import (
+	"database/sql"
+	"log"
+)
+
+var Db *sql.DB
+
+func setUp() {
+	Db, _ = sql.Open("sqlite3", ":memory:")
+	createTable(Db)
+	createProduct(Db)
+}
+
+func createTable(db *sql.DB) {
+	table := `CREATE TABLE products (
+		id string, 
+		name string, 
+		status string, 
+		price float);`
+
+	stmt, err := db.Prepare(table)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
+
+func createProduct(db *sql.DB) {
+	insert := `insert into products values 
+		("abc", "Product Test", "disabled", 0);`
+
+	stmt, err := db.Prepare(insert)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	stmt.Exec()
+}
