ae1dc06458d4532628ea5e13a647f32c27441081
Elton Bezerra <elton.cin@hotmail.com>
2022-11-02 15:44:20 -0300

    Unit testing Product struct

--- app/product_test.go
+++ app/product_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/Elton-Bezerra/ports-and-adapter/app"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,4 +21,49 @@ func TestProduct_Enable(t *testing.T) {
 	err = product.Enable()
 
 	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := app.Product{}
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := app.Product{}
+	product.ID = uuid.NewString()
+	product.Name = "Hello"
+	product.Status = app.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+
+	require.Nil(t, err)
+
+	product.Status = "abrobrinha"
+	_, err = product.IsValid()
+	require.Equal(t, "status must be enabled or disabled", err.Error())
+
+	product.Status = app.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal to 0", err.Error())
+
+	product.Price = 10
+	product.Name = ""
+	product.Status = ""
+	_, err = product.IsValid()
+	require.Equal(t, "Name: non zero value required", err.Error())
+
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/Elton-Bezerra/ports-and-adapter
 
 go 1.19
 
+require github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
+
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
