ed79fa327100497f6a0c91d3551c10c0a1928132
Elton Bezerra <elton.cin@hotmail.com>
2022-11-15 20:07:45 -0300

    feat: starting getProduct handler implementation

---
+++ adapters/web/server/handler/product.go
@@ -0,0 +1,31 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/Elton-Bezerra/ports-and-adapter/app"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service app.ProductServiceInterface) {
+	r.Handle("/product/{id}", n.With(
+		negroni.Wrap(getProduct(service)),
+	)).Methods("GET", "OPTIONS")
+}
+
+func getProduct(service app.ProductServiceInterface) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		vars := mux.Vars(r)
+		id := vars["id"]
+
+		product, err := service.Get(id)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+	})
+}
